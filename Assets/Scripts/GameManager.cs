f06e7caaab5c7ce726bb4a74030e11310f873735
Luong Cong Dan <dan.luongcong@gameloft.com>
2022-07-21 10:48:42 +0700

    - test character

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,11 +13,15 @@ public class GameManager : MonoBehaviour
 
     [SerializeField]
     private Image image;
+
     private Heart heart;
     private HeartContainer heartContainer;
+    private Player player;
 
     void Awake()
     {
+        player = new Player(20, 20);
+
         heart = new Heart(image);
 
         heartContainer = new HeartContainer(
@@ -25,19 +29,24 @@ public class GameManager : MonoBehaviour
                 .Select(image => new Heart(image))
                 .ToList()
         );
+
+        player.OnHealed += (sender, args) => heartContainer.Replenish(args.Amount);
+        player.OnDamaged += (sender, args) => heartContainer.Deplete(args.Amount);
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            heart.Replenish(amount);
-            heartContainer.Replenish(amount);
+            // heart.Replenish(amount);
+            // heartContainer.Replenish(amount);
+            player.Heal(amount);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            heart.Deplete(amount);
-            heartContainer.Deplete(amount);
+            // heart.Deplete(amount);
+            // heartContainer.Deplete(amount);
+            player.Damage(amount);
         }
     }
 }
